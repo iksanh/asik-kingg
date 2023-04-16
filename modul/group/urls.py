3374e94ba67bda686055e343989149869fd355cc
iksanh <iksanhariji@gmail.com>
2023-04-17 06:40:26 +0800

    add url delete in group urls

--- modul/group/urls.py
+++ modul/group/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import MyGroupView, MyGroupCreateView, MyGroupUpdateView
+from .views import MyGroupView, MyGroupCreateView, MyGroupUpdateView, delete_group
 
 urlpatterns = [
     path('list', MyGroupView.as_view(), name='group'),
     path('create', MyGroupCreateView.as_view(), name='create-group'),
     path('<int:pk>/edit/', MyGroupUpdateView.as_view(), name='edit-group'),
+    path('<int:id>/delete/', delete_group, name= 'delete-group')
 ]
