a28943b3bb526ea873930275cbcbfdff6674c633
iksanh <iksanhariji@gmail.com>
2023-04-17 06:44:14 +0800

    maintain form User

--- modul/users/forms.py
+++ modul/users/forms.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User, Group, Permission
 
 class RegisterUserForm(UserCreationForm):
     qs_group = Group.objects.all()
-    group = forms.ModelMultipleChoiceField(queryset=qs_group, widget=forms.CheckboxSelectMultiple)
+    groups = forms.ModelMultipleChoiceField(queryset=qs_group, widget=forms.CheckboxSelectMultiple, required=False)
     email = forms.EmailField(max_length=255)
 
     def __init__(self, *args, **kwargs):
@@ -17,12 +17,12 @@ class RegisterUserForm(UserCreationForm):
         instance = super().save(commit=commit)
         instance.groups.clear()
 
-        for option_group in self.cleaned_data['group']:
+        for option_group in self.cleaned_data['groups']:
             instance.groups.add(option_group)
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2', 'group')
+        fields = ('username', 'email', 'password1', 'password2', 'groups')
 
 
 
