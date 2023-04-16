bacab87d52e8fca0d631138b4f7a56dad7b10b69
iksanh <iksanhariji@gmail.com>
2023-04-17 06:39:34 +0800

    repair url users

--- modul/users/urls.py
+++ modul/users/urls.py
@@ -1,12 +1,15 @@
 from django.urls import  path
-from .views import MyLoginView,  MyPermisionView,  MyUserView, MyUserRegistrationView
+from .views import MyLoginView,  MyPermisionView,  MyUserView, MyUserRegistrationView, MyUserUpdateView, inactive_user
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('login/', MyLoginView.as_view(template_name = "users/login.html"), name= 'login'),
     path('logout/', LogoutView.as_view(next_page='login'), name= 'logout'),
-    path('users/', MyUserView.as_view(), name= 'users'),
-    path('users/create', MyUserRegistrationView.as_view(), name = 'create-users'),
+    path('list/', MyUserView.as_view(), name= 'users'),
+    path('create', MyUserRegistrationView.as_view(), name='create-users'),
+    path('<int:pk>/edit/', MyUserUpdateView.as_view(), name='edit-users'),
+    path('<int:id>/status', inactive_user, name = 'inactive_user' ),
+    # path('<int:id>/edit/', user_update, name='edit-users'),
 
-    path('user/permission', MyPermisionView.as_view(), name= 'user-permission' )
+    path('user/permission', MyPermisionView.as_view(), name='user-permission')
 ]
