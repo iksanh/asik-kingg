a8a49856c56dacd83d72e0a7a2351e46a9482899
iksanh <iksanhariji@gmail.com>
2023-04-26 06:31:34 +0800

    add modul akun

--- modul/transaksi/urls.py
+++ modul/transaksi/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
 from modul.transaksi import views as view
 
+
 urlpatterns = [
-    path('', view.list_akun, name='akun'),
+    path('', view.AkunView.as_view(), name='list-akun'),
     path('create/', view.create_akun, name='create-akun'),
     path('edit/<int:id>', view.edit_akun, name='edit-akun'),
-    path('delete/<int:id>', view.delete_akun, name='delete-akun')
+    path('delete/<int:id>', view.delete_akun, name='delete-akun'),
+
+    ## url for angsuran 
+    path('angsuran/', view.AngsuranView.as_view(), name='list-angsuran'),
+    path('create/angsuran', view.AngsuranCreate.as_view(), name='create-angsuran'),
+    path('edit/angsuran/<int:pk>', view.AngsuranUpdate.as_view(), name='edit-angsuran'),
+    path('delete/angsuran/<int:id>', view.angsuran_delete, name='delete-angsuran')
+
 ]
--- modul/transaksi/views.py
+++ modul/transaksi/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView
+from modul.crud_params import CrudParams
 from .forms import AkunForm, AngsuranForm
 from .models import Akun, Angsuran
 
 # Create your views here.
+akun_params = CrudParams('akun')
+angsuran_params = CrudParams('angsuran')
+
+class AkunView(ListView):
+    model = Akun
+    template_name = 'transaksi/akun/list_akun.html'
+    context_object_name = 'data'
+    extra_context = akun_params.parameters(data_master=True, akun=True)
 
 def list_akun(request):
     template_list =  'transaksi/akun/list_akun.html'
@@ -16,41 +27,64 @@ def create_akun(request):
 
     if request.method == 'GET':
         form = AkunForm()
-        context = {'form': form}
+        context = akun_params.parameters(form=form, data_master=True, akun=True, action='Buat Data') 
         return render(request, template_create, context)
     elif request.method == 'POST':
         form= AkunForm(request.POST)
+        context = akun_params.parameters(form=form, data_master=True, akun=True, action='Buat Data') 
         if form.is_valid():
             form.save()
-            return redirect('akun')
+            return redirect('list-akun')
         else:
-            return render(request, template_create, {'form': form})
+            return render(request, template_create, context)
+
 
 def delete_akun(request, id):
     akun = get_object_or_404(Akun, id= id)
     akun.delete()
-    return redirect('akun')
+    return redirect('list-akun')
 
 def edit_akun(request, id):
     template_create = 'transaksi/akun/create_akun.html'
     akun = get_object_or_404(Akun, id=id)
 
-    context = {
-        'form': AkunForm(instance=akun),
-        'id': id
-    }
+    context = akun_params.parameters(form=AkunForm(instance=akun), id=id, data_master=True, akun=True, action='Edit Data') 
     if request.method == 'GET':
         return render(request, template_create,  context)
 
     elif request.method == 'POST':
         form = AkunForm(request.POST, instance=akun)
+        context = akun_params.parameters(form=form, data_master=True, akun=True, action='Edit Data')
         if form.is_valid():
             form.save()
-            return redirect('akun')
+            return redirect('list-akun')
         else:
-            return render(request, template_create, {'form', form})
+            return render(request, template_create, context)
 
 
 
 
+class AngsuranView(ListView):
+    model = Angsuran
+    template_name = 'transaksi/angsuran/list_angsuran.html'
+    context_object_name = 'data'
+    extra_context = angsuran_params.parameters(data_master=True, angsuran=True)
 
+class AngsuranCreate(CreateView):
+    model = Angsuran
+    template_name = 'transaksi/angsuran/create_angsuran.html'
+    fields = '__all__'
+    extra_context = angsuran_params.parameters(data_master=True, angsuran=True, action='Buat Data')
+    success_url = reverse_lazy('list-angsuran')  
+
+class AngsuranUpdate(UpdateView):
+    model = Angsuran
+    fields = '__all__'
+    template_name = 'transaksi/angsuran/create_angsuran.html'
+    extra_context = angsuran_params.parameters(data_master=True, angsuran=True, action='Edit Data')
+    success_url = reverse_lazy('list-angsuran') 
+
+def angsuran_delete(request, id):
+    angsuran = get_object_or_404(Angsuran, id= id)
+    angsuran.delete()
+    return redirect('list-angsuran')
